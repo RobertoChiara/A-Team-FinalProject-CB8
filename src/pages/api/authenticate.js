8c019232b5d63718735a5c52ff28b43028c59950
RobertoChiara <148492123+RobertoChiara@users.noreply.github.com>
2024-04-19 18:18:46 +0200

    bugs fixed

--- src/pages/api/authenticate.js
+++ src/pages/api/authenticate.js
@@ -5,13 +5,13 @@ const authenticationToken = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) {
-    return res.status(401).json({ message: "Inserire un token valido." });
+    return res.status(401).json({ message: "Insert a valid Token" });
   } else {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err)
         return res
           .status(403)
-          .json({ message: "Non hai i permessi di verifica." });
+          .json({ message: "You don't have verification permission" });
       req.user = user;
       next();
     });
